d9a1f8dc072512619cffb2e05e89c01fdf5b90c0
markkypanc <rphanchotong@gmail.com>
2021-02-19 20:08:47 +0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Mark Rachapoom on 19/2/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
