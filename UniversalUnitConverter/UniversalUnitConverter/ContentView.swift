fbe4f2aa70bceac8f5cc017ba34bb0e0bba9e6b2
markkypanc <rphanchotong@gmail.com>
2021-02-20 02:19:13 +0700

    Add new project

---
+++ UniversalUnitConverter/UniversalUnitConverter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UniversalUnitConverter
+//
+//  Created by Mark Rachapoom on 20/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UniversalUnitConverter/UniversalUnitConverter/UniversalUnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversalUnitConverterApp.swift
+//  UniversalUnitConverter
+//
+//  Created by Mark Rachapoom on 20/2/21.
+//
+
+import SwiftUI
+
+@main
+struct UniversalUnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
