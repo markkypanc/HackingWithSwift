0a6601809a847f8ebfd1af7027d8f5ce0f7168c7
markkypanc <rphanchotong@gmail.com>
2021-03-01 23:40:22 +0700

    Finish UI Design

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -7,26 +7,61 @@
 
 import SwiftUI
 
+extension Color {
+    static let dropShadow = Color(hex: "aeaec0").opacity(0.6)
+    static let dropLight = Color(hex: "ffffff")
+}
+
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        scanner.scanLocation = 0
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
+
+        let r = (rgbValue & 0xff0000) >> 16
+        let g = (rgbValue & 0xff00) >> 8
+        let b = rgbValue & 0xff
+
+        self.init(red: Double(r) / 0xff, green: Double(g) / 0xff, blue: Double(b) / 0xff)
+    }
+}
+
 struct ContentView: View {
     
     var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
     var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        VStack {
-            VStack(spacing: 30) {
-                Text("Tap the flag of")
-                Text(countries[correctAnswer])
-            }
+        
+        ZStack {
             
-            ForEach(0 ..< 3) { number in
-                Button(action: {
-                   // flag was tapped
-                }) {
-                    Image(self.countries[number])
-                        .renderingMode(.original)
+            Color(.white)
+                .ignoresSafeArea(.all)
+            
+            VStack(spacing: 30) {
+                
+                VStack {
+                    Text("Tap th flag of")
+                        .font(.title)
+                    Text(countries[correctAnswer])
+                        .font(.system(size: 20))
+                }
+                .foregroundColor(.black)
+                
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        // Action when button got pressed
+                    }, label: {
+                        Image(self.countries[number])
+                            .resizable()
+                    })
+                    .cornerRadius(17)
+                    .shadow(color: .dropShadow, radius: 15, x: 10, y: 10)
+                    .shadow(color: .dropLight, radius: 15, x: -10, y: -10)
                 }
             }
+            .padding(.all, 70)
         }
     }
 }
